a47c3ea6b31b80ad0cb5e4080a6cfef9f1c2b218
skyhhaad <skyhhaad@example.com>
2024-01-08 15:29:52 +0800

    update proguard.jp

--- src/main/kotlin/top/takuron/ncpuy/tools/proguard/Main.kt
+++ src/main/kotlin/top/takuron/ncpuy/tools/proguard/Main.kt
@@ -4,13 +4,14 @@ import top.takuron.ncpuy.tools.utils.TerminalUtils
 
 const val TERMINAL_TITLE = "Choose proguard rules you wants to generate"
 fun main(args: Array<String>) {
-    TerminalUtils.buildMenu(listOf("Custome","Japanese hiragana","German","Greek","Russian"),{
+    TerminalUtils.buildMenu(listOf("Custome","Japanese katakana","Japanese hiragana","German","Greek","Russian"),{
         when(it){
             0 -> personalProgurad()
-            1 -> SpecificCharacterProguard("proguard-jp.txt", "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん").create()
-            2 -> SpecificCharacterProguard("proguard-ge.txt", "AaÄäBbCcDdEeFfGgHhIiJjKkLlMmNnOoÖöPpQqRrSsẞßTtUuÜüVvWwXxYyZz").create()
-            3 -> SpecificCharacterProguard("proguard-gr.txt", "ΑαΒβΓγΔδΕεϵϜϝΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσςΤτΥυΦφϕΧχΨψΩω").create()
-            4 -> SpecificCharacterProguard("proguard-ru.txt", "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя").create()
+            1 -> SpecificCharacterProguard("proguard-jp-ka.txt", "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん").create()
+            2 -> SpecificCharacterProguard("proguard-jp-hr.txt", "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン").create()
+            3 -> SpecificCharacterProguard("proguard-ge.txt", "AaÄäBbCcDdEeFfGgHhIiJjKkLlMmNnOoÖöPpQqRrSsẞßTtUuÜüVvWwXxYyZz").create()
+            4 -> SpecificCharacterProguard("proguard-gr.txt", "ΑαΒβΓγΔδΕεϵϜϝΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσςΤτΥυΦφϕΧχΨψΩω").create()
+            5 -> SpecificCharacterProguard("proguard-ru.txt", "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя").create()
         }
     }, TERMINAL_TITLE)
 }
